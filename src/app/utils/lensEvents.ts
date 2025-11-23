cb8dd819886322a09b80a01fa116073b2d274570
WebzioInfo <webzio.info@gmail.com>
2025-11-23 22:31:33 +0530

    quatation basic added3

--- src/app/utils/lensEvents.ts
+++ src/app/utils/lensEvents.ts
@@ -1,28 +1,28 @@
-// type LensEvents = {
-//   follow: boolean;
-//   move: { x: number; y: number };
-// };
+type LensEvents = {
+  follow: boolean;
+  move: { x: number; y: number };
+};
 
-// type Handler<T> = (payload: T) => void;
+type Handler<T> = (payload: T) => void;
 
-// class EventBus<E extends Record<string, unknown>> {
-//   private listeners: {
-//     [K in keyof E]?: Handler<E[K]>[];
-//   } = {};
+class EventBus<E extends Record<string, unknown>> {
+  private listeners: {
+    [K in keyof E]?: Handler<E[K]>[];
+  } = {};
 
-//   on<K extends keyof E>(event: K, handler: Handler<E[K]>) {
-//     (this.listeners[event] ||= []).push(handler);
-//   }
+  on<K extends keyof E>(event: K, handler: Handler<E[K]>) {
+    (this.listeners[event] ||= []).push(handler);
+  }
 
-//   off<K extends keyof E>(event: K, handler: Handler<E[K]>) {
-//     const arr = this.listeners[event];
-//     if (!arr) return;
-//     this.listeners[event] = arr.filter((h) => h !== handler);
-//   }
+  off<K extends keyof E>(event: K, handler: Handler<E[K]>) {
+    const arr = this.listeners[event];
+    if (!arr) return;
+    this.listeners[event] = arr.filter((h) => h !== handler);
+  }
 
-//   emit<K extends keyof E>(event: K, payload: E[K]) {
-//     this.listeners[event]?.forEach((handler) => handler(payload));
-//   }
-// }
+  emit<K extends keyof E>(event: K, payload: E[K]) {
+    this.listeners[event]?.forEach((handler) => handler(payload));
+  }
+}
 
-// export const lensEvents = new EventBus<LensEvents>();
+export const lensEvents = new EventBus<LensEvents>();
